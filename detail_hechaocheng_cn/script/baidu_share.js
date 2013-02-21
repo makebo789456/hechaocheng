ea74e1a964b18cec775e21419188b7e1bee5e374
acity-waiting <acity@hechaocheng.cn>
2013-02-21 20:43:33 +0800

    commit messages

--- detail_hechaocheng_cn/script/baidu_share.js
+++ detail_hechaocheng_cn/script/baidu_share.js
@@ -1,11 +1,11 @@
-// JavaScript Document
+ï»¿// JavaScript Document
 document.writeln("<div id=\"bdshare\" class=\"bdshare_t bds_tools get-codes-bdshare\">");
 document.writeln("<a class=\"bds_qzone\"></a>");
 document.writeln("<a class=\"bds_tsina\"></a>");
 document.writeln("<a class=\"bds_tqq\"></a>");
 document.writeln("<a class=\"bds_renren\"></a>");
 document.writeln("<a class=\"bds_t163\"></a>");
-document.writeln("<span class=\"bds_more\">¸ü¶à</span>");
+document.writeln("<span class=\"bds_more\">æ›´å¤š</span>");
 document.writeln("<a class=\"shareCount\"></a>");
 document.writeln("</div>");
 document.writeln("<script type=\"text/javascript\" id=\"bdshare_js\" data=\"type=tools&amp;uid=2868551\" ></script>");
